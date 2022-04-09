8c7edc871e8c4749da4ad6c70723b6104a168f74
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-04-09 17:35:30 -0400

    code cleanup

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,72 +1,60 @@
-
-const indexedDB =
-window.indexedDB ||
-window.mozIndexedDB ||
-window.webkitIndexedDB ||
-window.msIndexedDB ||
-window.shimIndexedDB;
-
 let db;
+const request = indexedDB.open('budget_tracker', 1);
+const indexedDB =
+  window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
 
-const request = indexedDB.open('budget_tracker', 1)
-
-request.onupgradeneeded = function(event) {
-    const db = event.target.result;
-    db.createObjectStore('new_item', { autoIncrement: true})
-}
-
-request.onsuccess = function(event) {
-    db = event.target.result;
-
-    if(navigator.onLine) {
-        uploadRecord();
-    }
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore('new_item', { autoIncrement: true });
 };
 
-request.onerror = function(event) {
-    console.log(event.target.errorCode)
-}
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadRecord();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
 
 function saveRecord(data) {
-    const transaction = db.transaction(['new_item'], 'readwrite');
-
-    const budgetObjectStore = transaction.objectStore('new_item');
-
-    budgetObjectStore.add(data);
-
+  const transaction = db.transaction(['new_item'], 'readwrite');
+  const budgetObjectStore = transaction.objectStore('new_item');
+  budgetObjectStore.add(data);
 }
 
 function uploadRecord() {
-    const transaction = db.transaction(['new_item'], 'readwrite');
+  const transaction = db.transaction(['new_item'], 'readwrite');
+  const budgetObjectStore = transaction.objectStore('new_item');
+  const getAll = budgetObjectStore.getAll();
 
-    const budgetObjectStore = transaction.objectStore('new_item');
-
-    const getAll = budgetObjectStore.getAll();
-
-    getAll.onsuccess = function() {
-        if(getAll.result.length > 0) {
-            fetch('/api/transaction', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                  }
-                })
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if(serverResponse.message) {
-                        throw new Error(serverResponse);
-                    }
-                    const transaction = db.transaction(['new_item'], 'readwrite');
-                    const budgetObjectStore = transaction.objectStore('new_item');
-                    budgetObjectStore.clear();
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        }
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['new_item'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('new_item');
+          budgetObjectStore.clear();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
 }
 
 window.addEventListener('online', uploadRecord);
